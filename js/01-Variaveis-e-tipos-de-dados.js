3da301fb0dc8db45c72ada44f8ff61d4f49ad7fb
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-04-01 15:37:21 -0300

    01 - Variáveis e tipos de dados

---
+++ js/01-Variaveis-e-tipos-de-dados.js
@@ -0,0 +1,63 @@
+/* -- Variáveis --*/
+var x;
+console.log(x); // undefined
+
+x = 0;
+console.log(x); // 0
+
+x = 1;
+console.log(x); // 1
+
+x = 1.2
+console.log(x); // 1.2
+
+var nomeQualquer = 10;
+console.log(nomeQualquer); // 10
+
+/* -- Tipos de dados --*/
+// - number
+// - string
+// - boolean
+// - null
+// - undefined
+// - object
+// - array
+
+x = 'Olá mundo';
+console.log(x); // Olá mundo
+x = "Olá Mundo";
+console.log(x); // Olá mundo
+
+x = true;
+console.log(x); // true
+x = false;
+console.log(x); // false
+
+x = null;
+console.log(x); // null
+
+x = undefined;
+console.log(x); // undefined
+
+x = {};
+console.log(x); // {}
+
+pessoa = {altura:1.87, peso: 90};
+console.log(pessoa); // {altula: 1.87, peso: 90}
+console.log(pessoa.peso); // 90
+
+pessoa.altura = 1.60;
+console.log(pessoa.altura); // 1.6
+
+var numeros = [];
+numeros = [1,2,3,4,5,6];
+console.log(numeros); // [1, 2, 3, 4, 5, 6]
+console.log(numeros[0]); // 1
+console.log(numeros[1]); // 2
+console.log(numeros[2]); // 3
+
+// anotações de arrays em objetos
+console.log(pessoa['altura']); // 1.6
+console.log(pessoa.altura); // 1.6
+
+
