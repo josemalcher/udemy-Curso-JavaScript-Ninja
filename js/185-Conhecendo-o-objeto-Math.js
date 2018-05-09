110ce8b835cc7d5adf098eca72caf4ce7a842d44
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-05-09 01:23:21 -0300

    185 - Conhecendo o objeto Math

---
+++ js/185-Conhecendo-o-objeto-Math.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    var valor = 10.5;
+
+    console.log(Math.PI);// calculo com circuferência...
+    console.log(Math.abs(valor));
+    console.log(Math.ceil(valor)); //arredonda para cima
+    console.log(Math.floor(valor)); //arredonda para baixo
+    console.log(Math.round(valor)); //arredonda palo padrão!
+    /*
+        3.141592653589793
+        10.5
+        11
+        10
+        NaN
+*/
+    console.log(Math.sqrt(9)); // raiz quadrada = 3
+    console.log(Math.cbrt(9)); // raiz cubica
+
+    console.log(Math.max(9,20,10,5)); // raiz cubica
+    console.log(Math.min(9)); // raiz cubica
+    console.log(Math.random()); // raiz cubica
+
+})();
