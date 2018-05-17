6b32caf3c3b112fafb722581c44c903b4ab8a05f
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-05-17 00:34:40 -0300

    188. REST Api - Cors

--- js/186-Ajax.js
+++ js/186-Ajax.js
@@ -3,12 +3,13 @@
     //front end
 
     var ajax = new XMLHttpRequest();
-    ajax.open('GET', 'http://localhost:3000/user');
+    ajax.open('GET', 'http://localhost:3000/user/jose');
     ajax.send();
 
     ajax.addEventListener('readystatechange', function (e) {
-        if(ajax.readyState ===4 && ajax.status === 200){
-            console.log(ajax.responseText);
+        //if(ajax.readyState ===4 && ajax.status === 200){
+        if(ajax.readyState === 4 ){
+            console.log(ajax.responseText, ajax.status);
         }
     }, false)
 
--- rest-api/index.js
+++ rest-api/index.js
@@ -1,7 +1,18 @@
 'use strict';
 
 var express = require('express');
+var cors = require('cors');
 var app = express();
+
+var users = {
+    jose:{nome: 'Jose Malcher',
+    idade: 33
+    },
+    luci:{nome:'Luciana Barbosa',
+    idade:32}
+};
+
+app.use(cors());
 
 app.get('/', function (req, res) {
     res.send('<h1>Home</h1>');
@@ -9,6 +20,16 @@ app.get('/', function (req, res) {
 
 app.get('/user', function (req, res) {
    res.send('User');
+});
+
+app.get('/user/:username', function (req, res) {
+    var username = req.params.username;
+    if(users[username]){
+        return res.json(users[username]);
+    }
+    //res.json({error:'Usuário nâo encontrado'});
+    res.status(404).json({error:'Usuário nâo encontrado'});
+
 });
 
 app.listen(3000);
