2d102bacf3761671c61c3e443e18a01578ae098c
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-05-17 00:51:29 -0300

    189. Ajax - sync - POST - Sincrono

--- js/186-Ajax.js
+++ js/186-Ajax.js
@@ -3,15 +3,25 @@
     //front end
 
     var ajax = new XMLHttpRequest();
-    ajax.open('GET', 'http://localhost:3000/user/jose');
+    ajax.open('GET', 'http://localhost:3000/user/jose', true); // assincrono
+    //ajax.open('GET', 'http://localhost:3000/user/jose', false); //sincrono
     ajax.send();
 
-    ajax.addEventListener('readystatechange', function (e) {
+    //console.log(ajax.responseText, ajax.status);
+//186-Ajax.js:6 [Deprecation] Synchronous XMLHttpRequest on the main thread is deprecated because of its detrimental effects to the end user's experience. For more help, check https://xhr.spec.whatwg.org/.
+
+
+    /*ajax.addEventListener('readystatechange', function (e) {
         //if(ajax.readyState ===4 && ajax.status === 200){
-        if(ajax.readyState === 4 ){
+        if (ajax.readyState === 4) {
             console.log(ajax.responseText, ajax.status);
         }
-    }, false)
+    }, false);*/
+    ajax.onreadystatechange = function (e) {
+        if (ajax.readyState === 4) {
+            console.log(ajax.responseText, ajax.status);
+        }
+    };
 
 
 })();
